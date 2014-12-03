a093aeaea8f3442eb2e7efd44628650115f6b2a5
Tejong Lim <lim.tejong@gmail.com>
2014-12-03 15:17:30 -0600

    adding cities

---
+++ cities/main.js
@@ -0,0 +1,13 @@
+var fs = require('fs'),
+    dat = require('dat'),
+    db = dat(csvpipe),
+    file = 'cities.csv'; // name of csv file with dataset
+
+function csvpipe(err, test) {
+    console.log('test');
+    if (err) return console.error('Error in csvpipe', err);
+    var filedata = fs.createReadStream(file, {format: 'csv'});
+    var writeStream = db.createWriteStream({format: 'csv'});
+    filedata.pipe(writeStream);
+    console.log('All entries stored!');
+};
